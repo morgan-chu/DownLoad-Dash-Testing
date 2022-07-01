30b081e5219c9587e0568cc0b4e48f41dc6a7ccb
Morgan Chu <morganchu@Morgans-Macbook-Pro.local>
2022-07-01 15:02:57 -0400

    things work kinda

--- testing.py
+++ testing.py
@@ -1,7 +1,10 @@
-from dash import Dash, html, dcc, Input, Output, State, callback_context
+from dash import Dash, html, dcc, Input, Output, dash_table
 import plotly.express as px
 import pandas as pd
 from dash.dependencies import Input, Output
+
+
+#TODO: Running total of total volume, chart of time and urine amount, 
 
 app = Dash(__name__)
 df = pd.read_csv("data/SAMPLE_PATIENT_NAMES.csv")
@@ -9,21 +12,87 @@ print(df)
 app.layout = html.Div([
     html.H1(children='Project DownLoad Data Interface'),
 
-    html.H2(children = 'Room Number', style={}),
+    html.H2(children = 'Room Number', style={'margin-left' : "25px", 'text-align': 'center', 'width' : '20%'}),
 
     html.Div(id='Room_Number', children=[
-        dcc.Dropdown(df.get('Rm_Num'), id='pandas-dropdown-1', clearable = False, value = 1100),
-    ], style={'padding': 25,'flex': 1, 'display': 'inline-block', 'width': '20%'}),
+        dcc.Dropdown(df.get('Rm_Num'), id='Rm-Number', clearable = False, value = 1100),
+    ], style={'margin-left': "25px",'flex': 1, 'display': 'inline-block', 'width': '20%', "verticalAlign": "top"}),
 
-    html.Div(id='Patient_Name', style={'padding': 0, 'flex': 1, 'display': 'inline-block', 'width': '40%', 'text-align': 'center'}),
+    html.Div(id='Patient_Name_Group', children = [
+        html.H3(id = "Patient_Name"),
+    ], style={'margin-bottom': "15px", 'flex': 1, 'display': 'inline-block', 'width': '25%', 'text-align': 'center', "verticalAlign": "top"}),
+
+    html.Div(id='Current_Vol_Group', children = [
+        html.H3(id = "tot_Vol"),
+    ], style={'margin-bottom': "15px", 'flex': 1, 'display': 'inline-block', 'width': '25%', 'text-align': 'center', "verticalAlign": "top"}),
+
+    html.Div(id = 'Graph_Group', children = [
+        dcc.Graph(id='indicator-graphic'),
+        dcc.Interval(
+            id='interval-component',
+            interval=1*1000, # in milliseconds
+            n_intervals=0
+        )
+    ], style={'padding': '15px', 'display': 'inline-block', 'width':'45%'}),
+
+    html.Div(id='vol_table_group', children = [
+        html.Label(children = "Table of Urine Volumes", style={}),
+        html.Table(id = 'urine-vol-table'),
+    ], style={'padding': '15px', 'display': 'inline-block', 'width':'40%', 'vertical-align': 'top'}),
 ])
 
 @app.callback(
     Output('Patient_Name', 'children'),
-    Input('pandas-dropdown-1', 'value')
+    Input('Rm-Number', 'value'),
+    Input('interval-component', 'n_intervals')
 )
-def update_output(value):
+def update_output(value, intervals):
     return f'Patient Name: {df.at[value-1100, "Patient_Name"]}'
+
+@app.callback(
+    Output('tot_Vol', 'children'),
+    Input('Rm-Number', 'value'),    
+    Input('interval-component', 'n_intervals')
+)
+def update_output(rm_number, intervals):
+    df = pd.read_csv("data/" + str(rm_number) + ".csv")
+    dff = df['Volume(mL)']
+    sum = 0
+    for val in dff:
+        sum += val
+    return f'Total Urine Volume: {str(sum)} mL'
+
+@app.callback(
+    Output('indicator-graphic', 'figure'),
+    Input('Rm-Number', 'value'), 
+    Input('interval-component', 'n_intervals')
+)
+def update_graph(rm_number, intervals):
+    df = pd.read_csv("data/" + str(rm_number) + ".csv")
+    fig = px.line(df, x= "Time", y = "Volume(mL)", markers = True)
+    fig.update_layout(title_text='Urine Volume Over Time', title_x=0.5)
+
+    return fig
+
+@app.callback(
+    Output('urine-vol-table', 'children'),
+    Input('Rm-Number', 'value'), 
+    Input('interval-component', 'n_intervals')
+)
+def generate_table(rm_number, intervals, max_rows=10):
+    dataframe = pd.read_csv("data/" + str(rm_number) + ".csv")
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+            )
+        ,
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
